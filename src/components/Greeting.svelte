2aa097c9397898f2380f52d0d33014cba8edc2f3
fran sola <fmia1992@gmail.com>
2023-12-02 14:10:46 +0100

    primeros pasos completados

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const hours = currentTime.getHours()
+    
+    let greeting = ''
+
+    if (hours < 12) {
+        greeting = 'Buenos días'
+    } else if (hours < 18) {
+        greeting = 'Buenas tardes'
+    } else {
+        greeting = 'Buenas noches'
+    }
+</script>
+
+<h1 class="text-3xl font-bold">
+    {greeting}
+</h1>
